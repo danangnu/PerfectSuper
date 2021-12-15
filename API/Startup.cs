5273eadf7f83c023db77895c658180fadf4d8cc1
Danang Nurdiansyah <nattesc82@gmail.com>
2021-12-15 16:53:02 +0800

    master

--- API/Startup.cs
+++ API/Startup.cs
@@ -72,12 +72,12 @@ namespace API
         {
             app.UseMiddleware<ExceptionMiddleware>();
 
-            // app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            // app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            // app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
             app.UseAuthorization();
 
